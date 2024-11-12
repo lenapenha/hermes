9bc070aa4c3dcc1d2a455a6b34b9c43dfb905311
Helena Penha <lenapenha02@hotmail.com>
2024-11-11 21:33:56 -0600

    turn ErrorResponse in record instead class

--- src/main/java/com/lenap/hermes/exception/ExceptionHandlerController.java
+++ src/main/java/com/lenap/hermes/exception/ExceptionHandlerController.java
@@ -26,22 +26,6 @@ public class ExceptionHandlerController {
                 .body(new ErrorResponse("Bad Request", ex.getMessage()));
     }
 
-    public static class ErrorResponse{
-        private final String message;
-
-        private final String details;
-
-        public ErrorResponse(String message, String details) {
-            this.message = message;
-            this.details = details;
-        }
-
-        public String getMessage() {
-            return message;
-        }
-
-        public String getDetails() {
-            return details;
-        }
+    public record ErrorResponse(String message, String details) {
     }
 }
