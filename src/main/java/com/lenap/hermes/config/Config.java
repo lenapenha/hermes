d87fa2a9c9d163a44e95038e614615176ea3edbf
Helena Penha <lenapenha02@hotmail.com>
2024-12-10 18:42:13 -0600

    add cors webconfig

--- src/main/java/com/lenap/hermes/config/Config.java
+++ src/main/java/com/lenap/hermes/config/Config.java
@@ -6,8 +6,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.core.env.Environment;
 
-import java.util.logging.Filter;
-
 @Configuration
 public class Config {
 
